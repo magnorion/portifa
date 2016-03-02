c8b2fd1ff89110d32e97905a1d69e1b27369e2bb
Lucas Dias Lopes <lucas_front@yahoo.com.br>
2016-03-02 18:45:25 -0300

    Segundo Commit

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -1,4 +1,14 @@
 var app = angular.module("app",[]);
 app.controller("app-controller",function($scope){
-  
+
+  // Boxs
+  $scope.boxes = [
+    { img:"assets/img/no-img.png" },
+    { img:"assets/img/no-img.png" },
+    { img:"assets/img/no-img.png" },
+    { img:"assets/img/no-img.png" },
+    { img:"assets/img/no-img.png" },
+    { img:"assets/img/no-img.png" }
+  ];
+  // endBoxs
 });
